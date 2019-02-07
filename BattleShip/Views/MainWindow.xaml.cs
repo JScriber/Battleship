1b441613724677896c09d2d45357f930673e28be
Alexis Pannetier <alexis.pannetier@hotmail.fr>
2019-02-07 14:34:38 +0100

    Views update

--- BattleShip/Views/MainWindow.xaml.cs
+++ BattleShip/Views/MainWindow.xaml.cs
@@ -32,12 +32,6 @@ namespace BattleShip.Views
         #endregion
 
         #region Variables
-        ObservableCollection<Game> games = new ObservableCollection<Game>();
-        ObservableCollection<Ship> ships = new ObservableCollection<Ship>();
-        ObservableCollection<Player> players = new ObservableCollection<Player>();
-        ObservableCollection<Map> map = new ObservableCollection<Map>();
-        ObservableCollection<Cell> coordinates = new ObservableCollection<Cell>();
-        ObservableCollection<Dimension> dimensions = new ObservableCollection<Dimension>();
 
         #endregion
 
@@ -61,48 +55,11 @@ namespace BattleShip.Views
             
             InitializeComponent();
             this.Content = new Settings();
-            
-            this.Test();
 
             InitializeComponent();
         }
 
-        private void Test()
-        {
-            Dimension dimension = new Dimension(10, 10);
-
-            // Builders.
-            var gb = new GameBuilder();
-            var sb = new ShipBuilder(dimension);
-
-            // Default configurations modified by the user.
-            List<ShipConfiguration> configurations = new List<ShipConfiguration>()
-            {
-                new ShipConfiguration(Models.ShipType.Carrier, new Dimension(2, 1), 2),
-                new ShipConfiguration(Models.ShipType.Cruiser, new Dimension(2, 1), 2),
-                new ShipConfiguration(Models.ShipType.Submarine, new Dimension(2, 1), 2),
-                new ShipConfiguration(Models.ShipType.Destroyer, new Dimension(2, 1), 2),
-                new ShipConfiguration(Models.ShipType.BattleShip, new Dimension(2, 1), 2),
-            };
-
-            // TODO: View modifies the configurations.
-            List<Ship> ships = new List<Ship>()
-            {
-                sb.FromConfiguration(configurations[0], 1, 1, false),
-                sb.FromConfiguration(configurations[1], 1, 1, true),
-                sb.FromConfiguration(configurations[2], 1, 1, true),
-            };
-
-            // Random placement for IA.
-            List<Ship> robotShips = sb.RandomFromConfigurations(configurations);
-
-            Game game = gb.CreateGame(configurations, ships, robotShips, dimension);
-
-            foreach (var ship in game.Human.Map.Ships)
-            {
-                System.Console.WriteLine(ship.Type);
-            }
-        }
+        
 
         /// <summary>
         /// Setups the default configuration.
--- BattleShip/Views/Settings.xaml.cs
+++ BattleShip/Views/Settings.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using BattleShip.Controllers;
+using BattleShip.Models;
+using BattleShip.Models.Utils;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -30,6 +34,12 @@ namespace BattleShip.Views
         #endregion
 
         #region Variables
+        ObservableCollection<Game> games = new ObservableCollection<Game>();
+        ObservableCollection<Ship> ships = new ObservableCollection<Ship>();
+        ObservableCollection<Player> players = new ObservableCollection<Player>();
+        ObservableCollection<Map> map = new ObservableCollection<Map>();
+        ObservableCollection<Cell> coordinates = new ObservableCollection<Cell>();
+        ObservableCollection<Dimension> dimensions = new ObservableCollection<Dimension>();
         #endregion
 
         #region Attributs
@@ -38,13 +48,15 @@ namespace BattleShip.Views
         #endregion
 
         #region Properties
+        public ObservableCollection<Ship> ShipType { get; set; }
+
         public int MapWidth
         {
             get { return mapWidth; }
             set
             {
                 mapWidth = value;
-                ResizeMap();
+                ResizeMap(this.gameGridPlayer1);
             }
         }
 
@@ -54,7 +66,7 @@ namespace BattleShip.Views
             set
             {
                 mapHeight = value;
-                ResizeMap();
+                ResizeMap(this.gameGridPlayer1);
             }
         }
         #endregion
@@ -74,22 +86,59 @@ namespace BattleShip.Views
         #endregion
 
         #region Functions
-        private void ResizeMap()
+        private void Test()
         {
-            this.gameGridPlayer1.Children.Clear();
-            this.gameGridPlayer1.ColumnDefinitions.Clear();
-            this.gameGridPlayer1.RowDefinitions.Clear();
+            Dimension dimension = new Dimension(10, 10);
+
+            // Builders.
+            var gb = new GameBuilder();
+            var sb = new ShipBuilder(dimension);
+
+            // Default configurations modified by the user.
+            List<ShipConfiguration> configurations = new List<ShipConfiguration>()
+            {
+                new ShipConfiguration(Models.ShipType.Carrier, new Dimension(2, 1), 2),
+                new ShipConfiguration(Models.ShipType.Cruiser, new Dimension(2, 1), 2),
+                new ShipConfiguration(Models.ShipType.Submarine, new Dimension(2, 1), 2),
+                new ShipConfiguration(Models.ShipType.Destroyer, new Dimension(2, 1), 2),
+                new ShipConfiguration(Models.ShipType.BattleShip, new Dimension(2, 1), 2),
+            };
+
+            // TODO: View modifies the configurations.
+            List<Ship> ships = new List<Ship>()
+            {
+                sb.FromConfiguration(configurations[0], 1, 1, false),
+                sb.FromConfiguration(configurations[1], 1, 1, true),
+                sb.FromConfiguration(configurations[2], 1, 1, true),
+            };
+
+            // Random placement for IA.
+            List<Ship> robotShips = sb.RandomFromConfigurations(configurations);
+
+            Game game = gb.CreateGame(configurations, ships, robotShips, dimension);
+
+            foreach (var ship in game.Human.Map.Ships)
+            {
+                System.Console.WriteLine(ship.Type);
+            }
+        }
+
+        private void ResizeMap(Grid gridName)
+        {
+            gridName.Children.Clear();
+            gridName.ColumnDefinitions.Clear();
+            gridName.RowDefinitions.Clear();
 
             for (int i = 0; i < this.MapHeight; i++)
             {
                 ColumnDefinition col = new ColumnDefinition();
-                this.gameGridPlayer1.ColumnDefinitions.Add(col);
+                gridName.ColumnDefinitions.Add(col);
             }
 
             for (int i = 0; i < this.MapWidth; i++)
             {
                 RowDefinition row = new RowDefinition();
-                this.gameGridPlayer1.RowDefinitions.Add(row);
+                gridName.RowDefinitions.Add(row);
             }
 
             Task.Factory.StartNew(() =>
@@ -105,7 +154,7 @@ namespace BattleShip.Views
                             Grid.SetColumn(btn, i);
                             Grid.SetRow(btn, j);
 
-                            this.gameGridPlayer1.Children.Add(btn);
+                            gridName.Children.Add(btn);
                         }));
                     }
                 }
